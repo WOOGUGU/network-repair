cd5d235c4eca36d89d8e172f005c71cbb5163c0b
ZBWKHH <592562672@qq.com>
2022-01-13 14:53:30 +0800

    （后台管理）登录拦截器添加、解决了派单时候的一些并发引发的异常，修改返回值时间的形式示例

---
+++ src/main/java/com/example/repair/interceptor/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.example.repair.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object jobNumber = session.getAttribute("jobNumber");
+        if (jobNumber != null)
+            return true;
+        else
+            request.setAttribute("msg", "请先登录！");
+        response.sendRedirect("/");
+        return false;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+
+
+}

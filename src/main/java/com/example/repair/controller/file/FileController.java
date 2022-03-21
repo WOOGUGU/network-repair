d7a7e544cb8c136187a3ae5cf6a25e16e2db7d8b
WOOGUGU <woogugu@outlook.com>
2022-03-21 16:35:48 +0800

    图片上传优化

--- src/main/java/com/example/repair/controller/file/FileController.java
+++ src/main/java/com/example/repair/controller/file/FileController.java
@@ -1,6 +1,7 @@
 package com.example.repair.controller.file;
 
 import com.example.repair.util.ResultCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -16,6 +19,7 @@ import java.util.UUID;
  * @author WOOGUGU
  */
 
+@Slf4j
 @RestController
 public class FileController {
 
@@ -24,11 +28,12 @@ public class FileController {
             @RequestParam(value = "file") MultipartFile file
     ) {
         if (file.isEmpty()) {
-            System.out.println("请选择图片");
+            log.error("无图片");
         }
         // 文件名
         String fileName = file.getOriginalFilename();
         // 后缀名
+        assert fileName != null;
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         // 新文件名
         fileName = UUID.randomUUID() + suffixName;
@@ -43,8 +48,35 @@ public class FileController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("保存图片至==>" + filePath);
+        log.info("保存图片至==>" + filePath);
         //返回图片名称
-        return ResultCode.getJson("https://cn-zz-bgp-4.natfrp.cloud:25363/" + fileName);
+        return ResultCode.getJson("https://cn-cd-dx-6.natfrp.cloud:19751/" + fileName);
+    }
+
+    @PostMapping("/filesUpload")
+    public Object filesUpload(
+            @RequestParam(value = "files") MultipartFile[] files
+    ) {
+        List<String> list = new ArrayList<>();
+        String fileName, suffixName, filePath;
+        for (int i = 0; i < files.length; i++) {
+            fileName = files[i].getOriginalFilename();
+            assert fileName != null;
+            suffixName = fileName.substring(fileName.lastIndexOf("."));
+            fileName = UUID.randomUUID() + suffixName;
+            filePath = System.getProperty("user.dir") + "/images/" + fileName;
+            java.io.File dest = new java.io.File(filePath);
+            if (!dest.getParentFile().exists()) {
+                dest.getParentFile().mkdirs();
+            }
+            try {
+                files[i].transferTo(dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            log.info("保存图片至==>" + filePath);
+            list.add("https://cn-cd-dx-6.natfrp.cloud:19751/" + fileName);
+        }
+        return ResultCode.getJson(list);
     }
 }
